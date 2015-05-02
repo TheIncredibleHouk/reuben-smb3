8c1a017a4e42345c2d0de74ba074e251e6a0682c
Travis Houk <dahrkdaiz@hotmail.com>
2015-05-02 08:08:51 -0400

    World model created

---
+++ Reuben.Model/World.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Reuben.Model
+{
+    [DataContract]
+    public class World
+    {
+        public World()
+        {
+            Pointers = new List<WorldPointer>();
+            Data = new byte[0x40, 0x1B];
+        }
+
+
+        [DataMember]
+        public Guid ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int GraphicsBankID { get; set; }
+
+        [DataMember]
+        public int MusicID { get; set; }
+
+        [DataMember]
+        public int Length { get; set; }
+
+        [DataMember]
+        public int PaletteID { get; set; }
+
+        [DataMember]
+        public List<WorldPointer> Pointers { get; set; }
+
+        [DataMember]
+        public byte[,] Data { get; set; }
+    }
+}
---
+++ Reuben.Model/WorldPointer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Reuben.Model
+{
+    [DataContract]
+    public class WorldPointer
+    {
+        [DataMember]
+        public Guid LevelID { get; set; }
+
+        [DataMember]
+        public int X { get; set; }
+
+        [DataMember]
+        public int Y { get; set; }
+    }
+}
